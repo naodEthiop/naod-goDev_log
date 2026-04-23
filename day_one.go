8da7b9542b387a1cf1126313ebab416838927a70
Fkremariam Fentahun (NAOD-ETHIOP) <fkremariamfentahun66@gmail.com>
2026-04-23 16:03:02 +0300

    feat: implement basic HTTP server with user CRUD endpoints

---
+++ day_one.go
@@ -0,0 +1,274 @@
+// package main
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"net/http"
+// 	"strconv"
+// 	"sync"
+// )
+
+// type User struct {
+// 	ID   int    `json:"id"`
+// 	Name string `json:"name"`
+// 	Email string `json:"email"`
+// }
+
+// var (
+// 	userCache  = make(map[int]User)
+// 	cacheMutex sync.RWMutex
+// 	nextID     = 1
+// )
+
+// func main() {
+// 	mux := http.NewServeMux()
+
+// 	mux.HandleFunc("POST /users", createUser)
+// 	mux.HandleFunc("GET /users/{id}", getUser)
+// 	mux.HandleFunc("DELETE /users/{id}", deleteUser)
+
+// 	fmt.Println("Server listening on :8080")
+
+// 	err := http.ListenAndServe(":8080", mux)
+// 	if err != nil {
+// 		fmt.Println("server error:", err)
+// 	}
+// }
+
+// func getUser(w http.ResponseWriter, r *http.Request) {
+// 	id, err := strconv.Atoi(r.PathValue("id"))
+// 	if err != nil {
+// 		http.Error(w, "invalid user id", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	cacheMutex.RLock()
+// 	user, ok := userCache[id]
+// 	cacheMutex.RUnlock()
+
+// 	if !ok {
+// 		http.Error(w, "user not found", http.StatusNotFound)
+// 		return
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json") // set the json repsonse header  the response body is JSON
+
+// 	err = json.NewEncoder(w).Encode(user)
+// 	if err != nil {
+// 		http.Error(w, "failed to encode user", http.StatusInternalServerError)
+// 		return
+// 	}
+// }
+
+// func deleteUser(w http.ResponseWriter, r *http.Request) {
+// 	id, err := strconv.Atoi(r.PathValue("id"))
+// 	if err != nil {
+// 		http.Error(w, "invalid user id", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	cacheMutex.Lock()
+// 	defer cacheMutex.Unlock()
+
+// 	if _, ok := userCache[id]; !ok {
+// 		http.Error(w, "user not found", http.StatusNotFound)
+// 		return
+// 	}
+
+// 	delete(userCache, id)
+// 	w.WriteHeader(http.StatusNoContent)
+// }
+
+// func createUser(w http.ResponseWriter, r *http.Request) {
+// 	defer r.Body.Close()
+
+// 	var user User
+// 	err := json.NewDecoder(r.Body).Decode(&user)
+
+// 	if err != nil {
+// 		http.Error(w, "invalid JSON body", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	if user.Name == "" {
+// 		http.Error(w, "name is required", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	cacheMutex.Lock()
+// 	user.ID = nextID
+// 	userCache[nextID] = user
+// 	nextID++
+// 	cacheMutex.Unlock()
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.WriteHeader(http.StatusCreated)
+
+// 	err = json.NewEncoder(w).Encode(user)
+// 	if err != nil {
+// 		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+// 		return
+// 	}
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"sync"
+)
+ type User struct{
+	ID int `json:"id"`
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Age int `json:"age"`
+ }
+var (
+	userCache = make(map[int]User)
+	cacheMutex  sync.RWMutex
+	nextID = 1
+
+)
+func main(){
+
+mux := http.NewServeMux()
+mux.HandleFunc("GET / ", rootHandler)
+mux.HandleFunc("GET /users", getAllUser)
+mux.HandleFunc("POST /users", createUser)
+mux.HandleFunc("GET /users/{id}", getUser)
+mux.HandleFunc("DELETE /users/{id}", deleteUser)
+mux.HandleFunc("GET /health" , healthHandler)
+fmt.Println("Server listening to :8080")
+
+err := http.ListenAndServe(":8080",mux)
+if err!= nil{
+	fmt.Println("Server error ",err)
+	return
+
+}
+}
+
+func rootHandler(w http.ResponseWriter , r *http.Request){
+	w.Write([]byte("Welcome to Go API"))
+}
+func healthHandler (w http.ResponseWriter ,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	w.Write([]byte(`{"status":"ok"}`))
+}
+func getAllUser (w http.ResponseWriter, r *http.Request){
+	cacheMutex.RLock()
+	users:= make([]User, 0 , len(userCache))
+
+	for _, user := range userCache{
+		users= append(users, user)
+	}
+
+	cacheMutex.RUnlock()
+
+	w.Header().Set("Content-Type", "application/json")
+   err := json.NewEncoder(w).Encode(users)
+   if err != nil{
+
+http.Error(w , "failed to encode users", http.StatusInternalServerError)
+return
+   }
+
+}
+
+func getUser(w http.ResponseWriter , r *http.Request){
+	id, err := strconv.Atoi( r.PathValue("id"))
+   if err != nil{
+	http.Error(w , "invalid user id ", http.StatusBadRequest)
+	return
+   }
+
+   cacheMutex.RLock()
+   user , ok := userCache[id]
+   cacheMutex.RUnlock()
+ if !ok {
+	http.Error(w, "user not found",http.StatusNotFound)
+	return
+
+ } 
+w.Header().Set("Content-Type","application/json")
+
+ err = json.NewEncoder(w).Encode(user)
+ if err !=nil{
+	http.Error(w,"failed to encode user", http.StatusInternalServerError)
+	return
+
+ }
+
+}
+
+
+func createUser(w http.ResponseWriter , r *http.Request){
+	defer r.Body.Close()
+  var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err!= nil{
+		http.Error(w , "invalid JSON body ",http.StatusBadRequest)
+		return
+	}
+	if user.Name ==""{
+		http.Error(w,"name is required",http.StatusBadRequest)
+		return
+	}
+	// creating user mechanism 
+	cacheMutex.Lock()
+	user.ID =nextID
+	userCache[nextID] = user
+   nextID ++
+   cacheMutex.Unlock()
+
+
+   w.Header().Set("Content-Type","application/json")
+   w.WriteHeader(http.StatusCreated)
+
+
+   err = json.NewEncoder(w).Encode(user)
+   if err != nil{
+	http.Error(w, "failed to encode response", http.StatusInternalServerError)
+	return
+   }
+	}
+
+func deleteUser(w http.ResponseWriter  , r *http.Request){
+ id , err := strconv.Atoi(r.PathValue("id"))
+ if err != nil{
+	http.Error(w, "invalid id ", http.StatusBadRequest)
+	return
+
+ }
+ cacheMutex.Lock()
+ defer cacheMutex.Unlock()
+
+ if _,ok := userCache[id];!ok{
+	http.Error(w, "user not found", http.StatusNotFound)
+	return
+
+ }
+ delete(userCache, id)
+
+ w.WriteHeader(http.StatusNoContent)
+
+
+}
